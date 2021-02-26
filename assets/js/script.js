f9136878d26daa94d49ac69ad24422b0f2ab3253
Tess Austin <tessaustin92@gmail.com>
2021-02-26 16:00:23 -0700

    current date displays

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//API Key
+var APIkey = "a17e1499228be1f9c294ac18b234c7d7";
+
+//display current date in title
+var currentDate = moment().format("L");
+$("#current-date").text("(" + currentDate + ")");
