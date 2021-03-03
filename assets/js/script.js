a17507942e0f765771492bf8b8b6c391998ba48f
Tess Austin <tessaustin92@gmail.com>
2021-03-03 11:13:09 -0700

    current date temp, hum, wind show

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,31 @@
 //API Key
 var APIkey = "a17e1499228be1f9c294ac18b234c7d7";
 
-//display current date in title
+//display current date next to current city
 var currentDate = moment().format("L");
 $("#current-date").text("(" + currentDate + ")");
 
-//clickable search btn
-//function search() {
-  $('#search-btn').on('click', function() {
-    citySearch = $('#search-city')
-      .val()
-      .trim();
+//clickable search btn w/input text
+$("#search-btn").on("click", function() {
+  citySearch = $("#search-city")
+    .val()
+    .trim();
 
-    if (citySearch === '') {
-      return;
-    }
-    $('#search-city').val('');
-    getWeather(citySearch);
-  });
-//}
+  if (citySearch === "") {
+    return;
+  }
+
+  $("#search-city").val("");
+  getWeather(citySearch);
+   
+});
 
 //retreive current weather
 function getWeather(search) {
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="
       + search
       + "&units=imperial"
-      + APIkey;
+      + "&appid=" + APIkey;
 
       $.ajax ({
         type: "get",
@@ -34,12 +34,21 @@ function getWeather(search) {
           console.log('ERROR');
         },
         success: function(val) {
-          console.log('SUCCESS');
+          console.log("-- || Open Weather Map Data || --");
+          console.log("City Name:", val.name);
+          console.log("Time of data calculation, unix, UTC:", val.dt);
+          console.log("Weather Icon:", val.weather[0].icon);
+          console.log("Temperature:", val.main.temp, "°F");
+          console.log("Humidity:", val.main.humidity, "%");
+          console.log("Wind Speed:", val.wind.speed, "MPH");
+
+          $("#current-city").text(val.name);
+          $("#current-temp").html(val.main.temp + ' °F');
+          $("#current-humidity").html(val.main.humidity + '%');
+          $("#current-wind-speed").html(val.wind.speed + ' MPH');
+
         }
       });
-
-
+ 
 
 };
-
-//getWeather(saltLakeCity);
