640cf3a9d9cee9b3166c2c9ad14e4e60cb4948dd
Tess Austin <tessaustin92@gmail.com>
2021-03-02 19:11:03 -0700

    search and input working

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,42 @@ var APIkey = "a17e1499228be1f9c294ac18b234c7d7";
 //display current date in title
 var currentDate = moment().format("L");
 $("#current-date").text("(" + currentDate + ")");
+
+//clickable search btn
+//function search() {
+  $('#search-btn').on('click', function() {
+    citySearch = $('#search-city')
+      .val()
+      .trim();
+
+    if (citySearch === '') {
+      return;
+    }
+    $('#search-city').val('');
+    getWeather(citySearch);
+  });
+//}
+
+//retreive current weather
+function getWeather(search) {
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="
+      + search
+      + "&units=imperial"
+      + APIkey;
+
+      $.ajax ({
+        type: "get",
+        url: queryURL,
+        error: function(val) {
+          console.log('ERROR');
+        },
+        success: function(val) {
+          console.log('SUCCESS');
+        }
+      });
+
+
+
+};
+
+//getWeather(saltLakeCity);
