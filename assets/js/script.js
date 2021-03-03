584f020483d05ef47b5d654d8fd4744d02da794f
Tess Austin <tessaustin92@gmail.com>
2021-03-03 16:08:34 -0700

    uv index w/color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 //API Key
 var APIkey = "a17e1499228be1f9c294ac18b234c7d7";
 
-
-//display current date next to current city
+//auto display of date when app is up
 var currentDate = moment().format("L");
 $("#current-date").text("(" + currentDate + ")");
 
@@ -26,7 +25,8 @@ function getWeather(search) {
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="
       + search
       + "&units=imperial"
-      + "&appid=" + APIkey;
+      + "&appid=" 
+      + APIkey;
 
       $.ajax ({
         type: "get",
@@ -43,8 +43,67 @@ function getWeather(search) {
           $("#current-temp").html(val.main.temp + " °F");
           $("#current-humidity").html(val.main.humidity + "%");
           $("#current-wind-speed").html(val.wind.speed + " MPH");
-        }
-      });
+        
+      var lat = val.coord.lat;
+      var lon = val.coord.lon;
+      var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+            console.log(uvQueryURL)
+          $.ajax ({
+            type: "get",
+            url: uvQueryURL,
+            error: function(data) {
+              console.log(data)
+            },
+            success: function(data) {
+              console.log(data)
+              $("#uv-index").html(data.value);
+
+              if (data.value < 3) {
+              $("#uv-index").addClass("low")
+              } 
+              else if (data.value < 6) {
+                $("#uv-index").addClass("med")
+              }
+              else if (data.value < 9) {
+                $("#uv-index").addClass("med-high")
+              }
+              else {
+                $("#uv-index").addClass("high")
+              }
+            },
+
+          })
+        },
+    });   
 
 
+
+
+
+
+
+
+              /*
+          if (history.indexOf(searchValue) === -1) {
+            history.push(searchValue);
+            window.localStorage.setItem("history", JSON.stringify(history));
+            console.log("if History:", history);
+            makeRow(searchValue);
+          }
+
+          // clear old content
+          $("#current-city").empty();
+          $("#five-day-forecast").empty();
+*/          
+
+
+/*        
+
+
+      storeHistory(name);
+
+
+      $("#weather-content").show();
+  })
+*/
 };
